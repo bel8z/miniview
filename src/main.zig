1e4b12f25354dd23016f242872d5bcdac4ec1136
Matteo <herbert71@hotmail.it>
2022-09-20 12:41:35 +0200

    Review of code structure

--- src/main.zig
+++ src/main.zig
@@ -6,13 +6,15 @@ const L = win32.L;
 
 const app_name = L("MiniView");
 
+//=== Infrastructure ===//
+
 // NOTE (Matteo): Kept static to allow for growing it without risk of smashing the stack
 var panic_buffer: [4096]u8 = undefined;
 
 pub fn main() void {
     // NOTE (Matteo): Errors are not returned from main in order to call our
     // custom 'panic' handler - see below.
-    innerMain() catch unreachable;
+    app.main() catch unreachable;
 }
 
 pub fn panic(err: []const u8, maybe_trace: ?*std.builtin.StackTrace) noreturn {
@@ -57,138 +59,150 @@ pub fn panic(err: []const u8, maybe_trace: ?*std.builtin.StackTrace) noreturn {
     std.os.abort();
 }
 
-/// Actual main procedure
-fn innerMain() anyerror!void {
-    const hinst = win32.getCurrentInstance();
+//=== Actual application ===//
 
-    const win_class = win32.WNDCLASSEXW{
-        .style = 0,
-        .lpfnWndProc = wndProc,
-        .hInstance = hinst,
-        .lpszClassName = app_name,
-        // Default arrow
-        .hCursor = win32.getDefaultCursor(),
-        // Don't erase background
-        .hbrBackground = null,
-        // No icons available
-        .hIcon = null,
-        .hIconSm = null,
-        // No menu
-        .lpszMenuName = null,
+const app = struct {
+    const Command = enum(u32) {
+        Open = 1,
     };
 
-    _ = try win32.registerClassExW(&win_class);
+    var image: ?*gdip.Image = null;
 
-    try win32.initBufferedPaint();
-    defer win32.deinitBufferedPaint();
+    pub fn main() anyerror!void {
+        // Register window class
+        const hinst = win32.getCurrentInstance();
 
-    const menu = try win32.createMenu();
-    try win32.appendMenu(menu, .{ .String = .{ .id = @enumToInt(Command.Open), .str = L("Open") } }, 0);
-
-    const win_flags = win32.WS_OVERLAPPEDWINDOW;
-    const win = try win32.createWindowExW(
-        0,
-        app_name,
-        app_name,
-        win_flags,
-        win32.CW_USEDEFAULT,
-        win32.CW_USEDEFAULT,
-        win32.CW_USEDEFAULT,
-        win32.CW_USEDEFAULT,
-        null,
-        menu,
-        hinst,
-        null,
-    );
-
-    var mv = try MiniView.init();
-    defer mv.deinit();
-    try setAppPtr(win, &mv);
-
-    _ = win32.showWindow(win, win32.SW_SHOWDEFAULT);
-    try win32.updateWindow(win);
-
-    var msg: win32.MSG = undefined;
-
-    while (true) {
-        win32.getMessageW(&msg, null, 0, 0) catch |err| switch (err) {
-            error.Quit => break,
-            else => return err,
+        const win_class = win32.WNDCLASSEXW{
+            .style = 0,
+            .lpfnWndProc = wndProc,
+            .hInstance = hinst,
+            .lpszClassName = app_name,
+            // Default arrow
+            .hCursor = win32.getDefaultCursor(),
+            // Don't erase background
+            .hbrBackground = null,
+            // No icons available
+            .hIcon = null,
+            .hIconSm = null,
+            // No menu
+            .lpszMenuName = null,
         };
 
-        _ = win32.translateMessage(&msg);
-        _ = win32.dispatchMessageW(&msg);
+        _ = try win32.registerClassExW(&win_class);
+
+        // Init buffered painting
+        try win32.initBufferedPaint();
+        defer win32.deinitBufferedPaint();
+
+        // Init GDI+
+        try gdip.init();
+        defer gdip.deinit();
+
+        // Create window
+        const menu = try win32.createMenu();
+        try win32.appendMenu(menu, .{ .String = .{ .id = @enumToInt(Command.Open), .str = L("Open") } }, 0);
+
+        const win_flags = win32.WS_OVERLAPPEDWINDOW;
+        const win = try win32.createWindowExW(
+            0,
+            app_name,
+            app_name,
+            win_flags,
+            win32.CW_USEDEFAULT,
+            win32.CW_USEDEFAULT,
+            win32.CW_USEDEFAULT,
+            win32.CW_USEDEFAULT,
+            null,
+            menu,
+            hinst,
+            null,
+        );
+
+        _ = win32.showWindow(win, win32.SW_SHOWDEFAULT);
+        try win32.updateWindow(win);
+
+        // Main loop
+        defer disposeImage();
+
+        var msg: win32.MSG = undefined;
+
+        while (true) {
+            win32.getMessageW(&msg, null, 0, 0) catch |err| switch (err) {
+                error.Quit => break,
+                else => return err,
+            };
+
+            _ = win32.translateMessage(&msg);
+            _ = win32.dispatchMessageW(&msg);
+        }
     }
-}
 
-fn wndProc(
-    win: win32.HWND,
-    msg: u32,
-    wparam: win32.WPARAM,
-    lparam: win32.LPARAM,
-) callconv(win32.WINAPI) win32.LRESULT {
-    var miniview = getAppPtr(MiniView, win) orelse return win32.defWindowProcW(win, msg, wparam, lparam);
+    fn wndProc(
+        win: win32.HWND,
+        msg: u32,
+        wparam: win32.WPARAM,
+        lparam: win32.LPARAM,
+    ) callconv(win32.WINAPI) win32.LRESULT {
+        switch (msg) {
+            win32.WM_CLOSE => win32.destroyWindow(win) catch unreachable,
+            win32.WM_DESTROY => win32.PostQuitMessage(0),
+            win32.WM_PAINT => {
+                if (win32.beginBufferedPaint(win)) |pb| {
+                    defer win32.endBufferedPaint(win, pb) catch unreachable;
+                    // Actual painting is application defined
+                    paint(pb, win) catch unreachable;
+                } else |_| unreachable;
+            },
+            win32.WM_COMMAND => {
+                if (wparam & 0xffff0000 == 0) {
+                    const command = @intToEnum(Command, wparam & 0xffff);
+                    switch (command) {
+                        .Open => open(win) catch unreachable,
+                    }
+                }
+            },
+            else => return win32.defWindowProcW(win, msg, wparam, lparam),
+        }
 
-    switch (msg) {
-        win32.WM_CLOSE => win32.destroyWindow(win) catch unreachable,
-        win32.WM_DESTROY => win32.PostQuitMessage(0),
-        win32.WM_PAINT => {
-            if (win32.beginBufferedPaint(win)) |pb| {
-                defer win32.endBufferedPaint(win, pb) catch unreachable;
-                // Actual painting is application defined
-                miniview.paint(pb, win) catch unreachable;
-            } else |_| unreachable;
-        },
-        win32.WM_COMMAND => {
-            if (wparam & 0xffff0000 == 0) {
-                miniview.processCommand(
-                    @intToEnum(Command, wparam & 0xffff),
-                    win,
-                ) catch unreachable;
-            }
-        },
-        else => return win32.defWindowProcW(win, msg, wparam, lparam),
+        return 0;
     }
 
-    return 0;
-}
-
-fn setAppPtr(win: win32.HWND, ptr: anytype) !void {
-    _ = try win32.setWindowLongPtrW(
-        win,
-        win32.GWL_USERDATA,
-        @intCast(isize, @ptrToInt(ptr)),
-    );
-}
-
-fn getAppPtr(comptime T: type, win: win32.HWND) ?*T {
-    if (win32.getWindowLongPtrW(win, win32.GWL_USERDATA)) |address| {
-        return @intToPtr(?*T, @intCast(usize, address));
-    } else |_| {
-        return null;
+    fn paint(pb: win32.PaintBuffer, win: win32.HWND) gdip.Error!void {
+        // TODO: Painting code goes here
+        _ = pb;
+        _ = win;
     }
-}
 
-const Image = opaque {};
+    fn open(win: win32.HWND) gdip.Error!void {
+        var file_buf = [_:0]u16{0} ** 1024;
+        var ofn = win32.OPENFILENAMEW{
+            .lpstrFile = &file_buf,
+            .nMaxFile = file_buf.len,
+            .lpstrFilter = L("Image files\x00*.bmp;*.png;*.jpg;*.jpeg;*.tiff\x00"),
+        };
 
-const Command = enum(u32) {
-    Open = 1,
+        if (try win32.getOpenFileName(&ofn)) {
+            var new_image: *gdip.Image = undefined;
+            try gdip.checkStatus(gdip.createBitmapFromFile(&file_buf, &new_image));
+
+            _ = win32.messageBoxW(win, &file_buf, app_name ++ L(": Image Loaded"), 0) catch
+                return error.Win32Error;
+
+            try disposeImage();
+            image = new_image;
+        }
+    }
+
+    fn disposeImage() gdip.Error!void {
+        if (image) |old_img| {
+            try gdip.checkStatus(gdip.disposeImage(old_img));
+        }
+    }
 };
 
-const MiniView = struct {
-    // GDI+ stuff
-    gdip_token: win32.ULONG_PTR = 0,
-    gdip_dll: win32.HMODULE,
-    gdip_startup: GdiplusStartup,
-    gdip_shutdown: GdiplusShutdown,
-    img_load: GdipCreateBitmapFromFile,
-    img_dispose: GdipDisposeImage,
+//=== GDI+ wrapper ===//
 
-    // App specific stuff
-    image: ?*Image = null,
-
-    const Self = @This();
-
+const gdip = struct {
     pub const Error = win32.Error || error{
         GenericError,
         InvalidParameter,
@@ -212,75 +226,10 @@ const MiniView = struct {
         ProfileNotFound,
     };
 
-    pub fn init() Error!Self {
-        var dll = win32.kernel32.LoadLibraryW(L("Gdiplus")) orelse return error.Win32Error;
-
-        var self = Self{
-            .gdip_dll = dll,
-            .gdip_startup = try win32.loadProc(GdiplusStartup, "GdiplusStartup", dll),
-            .gdip_shutdown = try win32.loadProc(GdiplusShutdown, "GdiplusShutdown", dll),
-            .img_load = try win32.loadProc(GdipCreateBitmapFromFile, "GdipCreateBitmapFromFile", dll),
-            .img_dispose = try win32.loadProc(GdipDisposeImage, "GdipDisposeImage", dll),
-        };
-
-        const input = GdiplusStartupInput{};
-        var output: GdiplusStartupOutput = undefined;
-        const status = self.gdip_startup(&self.gdip_token, &input, &output);
-        std.debug.assert(status == 0);
-
-        return self;
-    }
-
-    pub fn deinit(self: *Self) void {
-        self.disposeImage() catch {};
-        _ = self.gdip_shutdown(self.gdip_token);
-        _ = win32.kernel32.FreeLibrary(self.gdip_dll);
-    }
-
-    pub fn processCommand(self: *Self, command: Command, win: win32.HWND) Error!void {
-        switch (command) {
-            .Open => try self.open(win),
-        }
-    }
-
-    pub fn paint(self: *const Self, pb: win32.PaintBuffer, win: win32.HWND) Error!void {
-        // TODO: Painting code goes here
-        _ = self;
-        _ = pb;
-        _ = win;
-    }
-
-    fn open(self: *MiniView, win: win32.HWND) Error!void {
-        var file_buf = [_:0]u16{0} ** 1024;
-        var ofn = win32.OPENFILENAMEW{
-            .lpstrFile = &file_buf,
-            .nMaxFile = file_buf.len,
-            .lpstrFilter = L("Image files\x00*.bmp;*.png;*.jpg;*.jpeg;*.tiff\x00"),
-        };
-
-        if (try win32.getOpenFileName(&ofn)) {
-            var image: *Image = undefined;
-            try checkStatus(self.img_load(&file_buf, &image));
-
-            _ = win32.messageBoxW(win, &file_buf, app_name ++ L(": Image Loaded"), 0) catch
-                return error.Win32Error;
-
-            try self.disposeImage();
-            self.image = image;
-        }
-    }
-
-    fn disposeImage(self: *Self) Error!void {
-        if (self.image) |old_img| {
-            try checkStatus(self.img_dispose(old_img));
-        }
-    }
-
-    //=== Internal GDI+ implementation ===//
+    pub const Status = c_int;
+    pub const Image = opaque {};
 
     const WINGDIPAPI = win32.WINAPI;
-
-    const Status = c_int;
 
     const GdiplusStartupInput = extern struct {
         GdiplusVersion: u32 = 1,
@@ -294,39 +243,64 @@ const MiniView = struct {
         NotificationUnhook: ?*anyopaque,
     };
 
-    const GdiplusStartup = fn (token: *win32.ULONG_PTR, input: *const GdiplusStartupInput, output: *GdiplusStartupOutput) callconv(WINGDIPAPI) Status;
+    const GdiplusStartup = fn (
+        token: *win32.ULONG_PTR,
+        input: *const GdiplusStartupInput,
+        output: *GdiplusStartupOutput,
+    ) callconv(WINGDIPAPI) Status;
     const GdiplusShutdown = fn (token: win32.ULONG_PTR) callconv(WINGDIPAPI) Status;
     const GdipCreateBitmapFromFile = fn (filename: win32.LPCWSTR, image: **Image) callconv(WINGDIPAPI) Status;
     const GdipDisposeImage = fn (image: *Image) callconv(WINGDIPAPI) Status;
 
-    inline fn checkStatus(status: Status) Error!void {
-        if (status != 0) return mapError(status);
+    var token: win32.ULONG_PTR = 0;
+    var shutdown: GdiplusShutdown = undefined;
+    var createBitmapFromFile: GdipCreateBitmapFromFile = undefined;
+    var disposeImage: GdipDisposeImage = undefined;
+    var dll: win32.HMODULE = undefined;
+
+    pub fn init() Error!void {
+        dll = win32.kernel32.LoadLibraryW(L("Gdiplus")) orelse return error.Win32Error;
+        shutdown = try win32.loadProc(GdiplusShutdown, "GdiplusShutdown", dll);
+        createBitmapFromFile = try win32.loadProc(GdipCreateBitmapFromFile, "GdipCreateBitmapFromFile", dll);
+        disposeImage = try win32.loadProc(GdipDisposeImage, "GdipDisposeImage", dll);
+
+        const startup = try win32.loadProc(GdiplusStartup, "GdiplusStartup", dll);
+        const input = GdiplusStartupInput{};
+        var output: GdiplusStartupOutput = undefined;
+        const status = startup(&token, &input, &output);
+        try checkStatus(status);
     }
 
-    inline fn mapError(status: Status) Error {
-        return switch (status) {
-            1 => error.GenericError,
-            2 => error.InvalidParameter,
-            3 => error.OutOfMemory,
-            4 => error.ObjectBusy,
-            5 => error.InsufficientBuffer,
-            6 => error.NotImplemented,
-            7 => error.Win32Error,
-            8 => error.WrongState,
-            9 => error.Aborted,
-            10 => error.FileNotFound,
-            11 => error.ValueOverflow,
-            12 => error.AccessDenied,
-            13 => error.UnknownImageFormat,
-            14 => error.FontFamilyNotFound,
-            15 => error.FontStyleNotFound,
-            16 => error.NotTrueTypeFont,
-            17 => error.UnsupportedGdiplusVersion,
-            18 => error.GdiplusNotInitialized,
-            19 => error.PropertyNotFound,
-            20 => error.PropertyNotSupported,
-            21 => error.ProfileNotFound,
-            else => error.Win32Error,
-        };
+    pub fn deinit() void {
+        _ = shutdown(token);
+        _ = win32.kernel32.FreeLibrary(dll);
+    }
+
+    inline fn checkStatus(status: Status) Error!void {
+        switch (status) {
+            0 => return,
+            1 => return error.GenericError,
+            2 => return error.InvalidParameter,
+            3 => return error.OutOfMemory,
+            4 => return error.ObjectBusy,
+            5 => return error.InsufficientBuffer,
+            6 => return error.NotImplemented,
+            7 => return error.Win32Error,
+            8 => return error.WrongState,
+            9 => return error.Aborted,
+            10 => return error.FileNotFound,
+            11 => return error.ValueOverflow,
+            12 => return error.AccessDenied,
+            13 => return error.UnknownImageFormat,
+            14 => return error.FontFamilyNotFound,
+            15 => return error.FontStyleNotFound,
+            16 => return error.NotTrueTypeFont,
+            17 => return error.UnsupportedGdiplusVersion,
+            18 => return error.GdiplusNotInitialized,
+            19 => return error.PropertyNotFound,
+            20 => return error.PropertyNotSupported,
+            21 => return error.ProfileNotFound,
+            else => return error.Win32Error,
+        }
     }
 };
