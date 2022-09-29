17637ba54004c88e9e4eee29abc238a1c85a5284
Matteo <herbert71@hotmail.it>
2022-09-29 12:56:05 +0200

    Implemented a file iterator based on the win32 API

--- src/main.zig
+++ src/main.zig
@@ -117,18 +117,57 @@ const Bump = struct {
     }
 };
 
+const FileIter = struct {
+    find: win32.HANDLE = undefined,
+    pattern: [1024]u16 = undefined,
+    data: win32.WIN32_FIND_DATAW = undefined,
+
+    fn init(path: []const u16) !FileIter {
+        var self = FileIter{};
+        if (path.len > self.pattern.len - 16) return error.PathTooLong;
+
+        std.mem.copy(u16, self.pattern[0..], path);
+
+        var len = path.len - 1;
+        if (path[len] != '\\') len += 1;
+
+        const suffix = L("\\*");
+        std.mem.copy(u16, self.pattern[len..], suffix[0..]);
+        len += suffix.len;
+        self.pattern[len] = 0;
+
+        const pattern_str = self.pattern[0..len :0];
+
+        self.find = win32.kernel32.FindFirstFileW(pattern_str, &self.data);
+        if (self.find != win32.INVALID_HANDLE_VALUE) return self;
+
+        return error.Win32Error;
+    }
+
+    fn next(self: *FileIter) ?[:0]const u16 {
+        while (true) {
+            if (win32.kernel32.FindNextFileW(self.find, &self.data) == win32.FALSE) {
+                return null;
+            }
+
+            if (self.data.dwFileAttributes & win32.FILE_ATTRIBUTE_DIRECTORY == 0) {
+                const len = std.mem.indexOfScalar(u16, self.data.cFileName[0..], 0) orelse unreachable;
+                return self.data.cFileName[0..len :0];
+            }
+        }
+    }
+};
+
+const extensions = L("*.bmp;*.png;*.jpg;*.jpeg;*.tiff");
+
 const app = struct {
     const Command = enum(u32) {
         Open = 1,
     };
 
     var image: ?*gdip.Image = null;
-    var memory: Bump = undefined;
 
     pub fn main() anyerror!void {
-        // Setup memory
-        memory = try Bump.init();
-        defer memory.deinit();
 
         // Register window class
         const hinst = win32.getCurrentInstance();
@@ -286,18 +325,24 @@ const app = struct {
         }
     }
 
-    fn open(win: win32.HWND) gdip.Error!void {
-        var file_buf = [_:0]u16{0} ** 1024;
+    fn open(win: win32.HWND) !void {
+        var buffer = [_:0]u16{0} ** 1024;
         var ofn = win32.OPENFILENAMEW{
             .hwndOwner = win,
-            .lpstrFile = &file_buf,
-            .nMaxFile = file_buf.len,
-            .lpstrFilter = L("Image files\x00*.bmp;*.png;*.jpg;*.jpeg;*.tiff\x00"),
+            .lpstrFile = &buffer,
+            .nMaxFile = buffer.len,
+            .lpstrFilter = L("Image files\x00") ++ extensions ++ L("\x00"),
         };
 
         if (try win32.getOpenFileName(&ofn)) {
+            // const len = std.mem.lastIndexOfScalar(u16, &buffer, '\\') orelse unreachable;
+            // var iter = try FileIter.init(buffer[0..len]);
+            // while (iter.next()) |path| {
+            //     _ = try win32.messageBoxW(null, path, L(""), 0);
+            // }
+
             var new_image: *gdip.Image = undefined;
-            try gdip.checkStatus(gdip.createImageFromFile(&file_buf, &new_image));
+            try gdip.checkStatus(gdip.createImageFromFile(&buffer, &new_image));
 
             try disposeImage();
             image = new_image;
@@ -463,3 +508,29 @@ const gdip = struct {
         }
     }
 };
+
+//=== Testing ===//
+
+test "File iterator" {
+    var path_ptr: [*:0]u16 = undefined;
+
+    const hr = win32.shell32.SHGetKnownFolderPath(
+        &win32.FOLDERID_LocalAppData,
+        0,
+        null,
+        &path_ptr,
+    );
+
+    if (hr != 0) {
+        std.log.err("SHGetKnownFolderPath returned {x}", .{hr});
+        return error.Win32Error;
+    }
+
+    _ = try win32.messageBoxW(null, path_ptr, L("Path"), 0);
+
+    var iter = try FileIter.init(path_ptr[0..std.mem.len(path_ptr)]);
+
+    while (iter.next()) |path16| {
+        _ = try win32.messageBoxW(null, path16, L("Files"), 0);
+    }
+}
